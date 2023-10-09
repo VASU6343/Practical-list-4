5e3413ffde1dda4d45e3f04b44eab1f1804ef532
Vasu Sapariya <141891510+VASU6343@users.noreply.github.com>
2023-10-09 10:44:32 +0530

    List_4_no_2.cpp

---
+++ List_4_no_2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class item
+{
+	private:
+		static int number;
+		static int count;
+	public:
+		static void getdata()
+		{ 
+      cout<<"220130318016";
+      cout<<"Vasu Sapariya";
+			int a=0;
+			number=a;
+			count++;
+		}
+		static void get_count()
+		{
+				cout<<"Value of count ::"<<count<<endl;
+		}
+};
+int item :: number;
+int item :: count;
+int main()
+{
+
+	item ::getdata();
+	item ::getdata();
+	item ::getdata();
+	item :: get_count();
+	return 0;
+	
+}
