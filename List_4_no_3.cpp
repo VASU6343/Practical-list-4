fc6e3c7fca9ffe8e1855f1e3694f0697ac0196f2
Vasu Sapariya <141891510+VASU6343@users.noreply.github.com>
2023-10-09 10:27:00 +0530

    List_4_no_3.cpp

---
+++ List_4_no_3.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+class student
+{
+		private:
+			float spi;
+			string name;
+		public:
+			void getdata()
+		{
+			cout<<"Enter the Name::";
+			cin>>name;
+			cout<<"Enter the SPI::";
+			cin>>spi;
+		}
+		void putdata()
+		{
+			cout<<name<<endl;
+			cout<<spi<<endl;
+		}
+		float sp()
+		{
+			return spi;
+		}
+		string na()
+		{
+			return name;
+		}
+};
+int main()
+{
+	int i;
+	student s[10];
+	for(i=0;i<10;i++)
+	{
+		s[i].putdata();
+	}
+	for(i=0;i<10;i++)
+	{
+		s[i].getdata();
+	}
+	int high=0;
+	for(i=1;i<10;i++)
+	{
+		if(s[i].sp()>s[high].sp())
+		{
+			high=i;
+		}
+	}
+	cout<<"Highest spi and name is :: "<<s[high].sp()<<" & "<<s[high].na();
+	return 0;
+	
+}
