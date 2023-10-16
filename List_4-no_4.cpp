2ea6117def90f9e48ba5b2022add5bb7cacbf3c9
Vasu Sapariya <141891510+VASU6343@users.noreply.github.com>
2023-10-16 09:45:51 +0530

    List_4-no_4.cpp

---
+++ List_4-no_4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class number
+{
+	public :
+		void setdata(int a,int b);
+		int no_1,no_2;
+};
+void number :: setdata(int a,int b)
+{
+	cout<<"Enter the value of a ::";
+	cin>>a;
+	cout<<"Enter the value of b ::";
+	cin>>b;
+	no_1=a;
+	no_2=b;
+}
+int add(number n)
+{
+	return(n.no_1+n.no_2);
+};
+int main()
+{
+	number n1;
+	n1.setdata(n1.no_1,n1.no_2);
+	cout<<"addition ::"<<add(n1);
+	return 0;
+}
