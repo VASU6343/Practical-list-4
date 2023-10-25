386fd7b788e238a899a225dec126b43ec8086281
Vasu Sapariya <141891510+VASU6343@users.noreply.github.com>
2023-10-25 23:43:03 +0530

    List_4_no_8.cpp

---
+++ List_4_no_8.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class x
+{
+int a=5;
+friend class y;
+};
+class y
+{
+public:
+void display(x &x1)
+{
+cout<<"Value of A is ::"<<x1.a;
+}
+};
+int main()
+{
+x x1;
+y y1;
+y1.display(x1);
+return 0;
+}
