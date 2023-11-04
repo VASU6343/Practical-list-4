f1708bdffa050ae06111f59fcb4cd98ad5d0922a
Vasu Sapariya <141891510+VASU6343@users.noreply.github.com>
2023-11-04 11:35:35 +0530

    List_4_no_13.cpp

---
+++ List_4_no_13.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class rectangle
+{
+	int l,b;
+	public:
+		rectangle()
+		{
+      cout<<"220130318016";
+      cout<<"Vasu Sapariya";
+			cout<<"Enter length of Rectangle ::";
+			cin>>l;
+			cout<<"Enter Bredth of Rectangle ::";
+			cin>>b;
+			
+			cout<<"Area of Rectangle is ::"<<l*b;
+		}
+		
+		~rectangle(){}
+		
+};
+int main()
+{
+	rectangle r1;
+}
